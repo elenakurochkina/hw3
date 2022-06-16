c4469b42935ce6b98ebd2da51e59dab3d4453d88
Elena Kurochkina <e.kuro4kina@gmail.com>
2022-06-16 19:27:50 +0300

    task3 completed

---
+++ task3/program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт 
+//таблицу кубов чисел от 1 до N
+
+System.Console.Write("enter number N: ");
+string x = Console.ReadLine();
+int N = int.Parse(x);
+int a = 1;
+while (a <= N)
+{
+    System.Console.WriteLine(a*a*a); 
+    a++;
+}
