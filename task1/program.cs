afe838c749f9fe0c11bd281e82d329d0e8fb00d4
Elena Kurochkina <e.kuro4kina@gmail.com>
2022-06-16 18:28:00 +0300

    task1 completed

---
+++ task1/program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и 
+//проверяет, является ли оно палиндромом
+
+System.Console.Write("enter a five-digit number: ");
+string x = Console.ReadLine();
+int a = int.Parse(x);
+if (a / 10000 == a % 10 && a / 1000 % 10 == a / 1000 % 10) System.Console.WriteLine("число является палиндромом");
+else System.Console.WriteLine("число не является палиндромом");
